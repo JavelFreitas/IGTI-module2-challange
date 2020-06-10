e865ad177e6f9a0e9ff349d0167217258500b4fc
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-10 17:15:24 -0300

    Added sumValues It sums the value of the grades of a specific student and subject

--- src/routes.js
+++ src/routes.js
@@ -73,6 +73,22 @@ function deleteGrade(id){
     }
 }
 
+function sumGrades(student, subject){
+    try{
+        const allGrades = getGrades();
+    
+        const filteredGrades = allGrades.grades.filter(grade => grade.student === student && grade.subject === subject);
+        if(filteredGrades.length === 0){
+            throw 'Student or subject not found';
+        }
+        const sumValues = filteredGrades.reduce((acc = 0, cur) => acc + parseInt(cur.value), 0);
+        return sumValues;
+
+    }catch(error){
+        throw error;
+    }
+}
+
 router.post('/newGrade', (request, response) => {
     const { student, subject, type, value } = request.body;
 
@@ -92,6 +108,16 @@ router.get('/getGrade/:id', (request, response) => {
         response.json(gradeRequested);
     } catch (err) {
         response.status(404).send(err)
+    }
+});
+
+router.get('/valueSum', (request, response) => {
+    try{
+        const {student, subject} = request.body;
+        const totalValue = sumGrades(student, subject);
+        response.json({value: totalValue});
+    }catch(error){
+        response.status(404).send(error);
     }
 });
 
