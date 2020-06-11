e0c7b125e7f62212b25ed8f2a044f677beb36dff
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-11 14:16:15 -0300

    Added update feature

--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,7 @@ function saveGrade(grade) {
 
     grade.id = allGrades.nextId;
 
-    grade = {id: grade.id, ...grade};
+    grade = { id: grade.id, ...grade };
 
     allGrades.grades.push(grade);
     allGrades.nextId += 1;
@@ -36,78 +36,114 @@ function getGradeById(id) {
 
         console.log(filteredGrade);
 
-        
-        if (typeof filteredGrade !== 'undefined') {            
+
+        if (typeof filteredGrade !== 'undefined') {
             return filteredGrade;
         }
 
         throw 'Could not find this grade';
-        
+
     } catch (err) {
         throw err;
     }
 
 }
 
-function deleteGrade(id){
-    try{
+function deleteGrade(id) {
+    try {
         const allGrades = getGrades();
         const index = parseInt(id);
         let message = '';
         const deletedGrade = allGrades.grades.find(grade => grade.id === index);
-        if(typeof deletedGrade === 'undefined'){
+        if (typeof deletedGrade === 'undefined') {
             message = { error: 'Grade not found' }
             throw message;
         }
 
         const filteredGrades = allGrades.grades.filter(grade => grade.id !== index);
 
-        const remainingGrades = {nextId: (allGrades.nextId), grades: filteredGrades}
-        
+        const remainingGrades = { nextId: (allGrades.nextId), grades: filteredGrades }
+
         writeFile(remainingGrades);
-        message = { status: 'Deleted with success', deletedGrade}
+        message = { status: 'Deleted with success', deletedGrade }
         return message;
 
-    }catch(err){
+    } catch (err) {
         throw err;
     }
 }
 
-function sumGrades(student, subject){
-    try{
+function sumGrades(student, subject) {
+    try {
         const allGrades = getGrades();
-    
+
         const filteredGrades = allGrades.grades.filter(grade => grade.student === student && grade.subject === subject);
-        if(filteredGrades.length === 0){
+        if (filteredGrades.length === 0) {
             throw 'Student or subject not found';
         }
         const sumValues = filteredGrades.reduce((acc = 0, cur) => acc + parseInt(cur.value), 0);
         return sumValues;
 
-    }catch(error){
+    } catch (error) {
         throw error;
     }
 }
 
-function typeAverage(subject, type){
-    try{
+function typeAverage(subject, type) {
+    try {
         const allGrades = getGrades();
-    
+
         const filteredGrades = allGrades.grades.filter(grade => grade.type === type && grade.subject === subject);
 
-        if(filteredGrades.length === 0){
+        if (filteredGrades.length === 0) {
             throw 'Type or subject not found';
         }
 
         const sumValues = filteredGrades.reduce((acc = 0, cur) => acc + parseInt(cur.value), 0);
-    
-        const average = sumValues/filteredGrades.length;
+
+        const average = sumValues / filteredGrades.length;
 
         return average;
 
-    }catch(error){
+    } catch (error) {
         throw error;
     }
+}
+
+function getThreeBestGrades(subject, type) {
+    try {
+        const allGrades = getGrades();
+
+        const filteredGrades = allGrades.grades.filter(grade => grade.type === type && grade.subject === subject);
+
+        if (filteredGrades.length === 0) {
+            throw 'Type or subject not found';
+        }
+
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+function updateGrade(newGrade) {
+    try {
+        const allGrades = getGrades();
+        const gradeIndex = allGrades.grades.findIndex(grade => grade.id === newGrade.id);
+
+        if (gradeIndex === -1) {
+            throw `Id ${newGrade.id} does not exist`;
+        }
+
+        newGrade.timestamp = allGrades.grades[gradeIndex].timestamp;
+        allGrades.grades[gradeIndex] = newGrade;
+
+        writeFile(allGrades);
+    } catch (error) {
+        throw error;
+    }
+
+
 }
 
 router.post('/newGrade', (request, response) => {
@@ -133,30 +169,51 @@ router.get('/getGrade/:id', (request, response) => {
 });
 
 router.get('/valueSum', (request, response) => {
-    try{
-        const {student, subject} = request.body;
+    try {
+        const { student, subject } = request.body;
         const totalValue = sumGrades(student, subject);
-        response.json({value: totalValue});
-    }catch(error){
+        response.json({ value: totalValue });
+    } catch (error) {
         response.status(404).send(error);
     }
 });
 
 router.get('/typeAverage', (request, response) => {
-    try{
-        const {subject, type} = request.body;
+    try {
+        const { subject, type } = request.body;
         const average = typeAverage(subject, type);
-        response.json({average});
-    }catch(error){
-        response.json({error});
+        response.json({ average });
+    } catch (error) {
+        response.json({ error });
     }
 });
 
+router.get('/threeBest', (request, response) => {
+    try {
+        const { subject, type } = request.body;
+        const topThree = getThreeBestGrades(subject, type);
+        response.json({ topThree });
+    } catch (error) {
+        response.json({ error });
+    }
+});
+
+router.put('/updateGrade', (request, response) => {
+    try {
+        const { id, student, subject, type, value } = request.body;
+        const newGrade = { id: parseInt(id), student, subject, type, value: parseInt(value) };
+        updateGrade(newGrade);
+        response.json({ message: "Grade updated", newGrade });
+    } catch (error) {
+        response.status(404).json({ error });
+    }
+})
+
 router.delete('/deleteGrade/:id', (request, response) => {
-    try{
+    try {
         const deleteMessage = deleteGrade(request.params.id)
         response.json(deleteMessage);
-    }catch(err){
+    } catch (err) {
         response.status(404).send(err);
     }
 });
